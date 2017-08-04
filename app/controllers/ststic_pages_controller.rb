8ddad5f95af7d4d2cb4b24ece149d404e9229cee
tassaddar <tassaddar-90@yandex.ru>
2017-08-04 13:50:26 +0300

    change the name of the controller

--- app/controllers/ststic_pages_controller.rb
+++
@@ -1,7 +0,0 @@
-class StsticPagesController < ApplicationController
-  def home
-  end
-
-  def help
-  end
-end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
-class StsticPagesControllerTest < ActionDispatch::IntegrationTest
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get ststic_pages_home_url
+    get static_pages_home_url
     assert_response :success
   end
 
   test "should get help" do
-    get ststic_pages_help_url
+    get static_pages_help_url
     assert_response :success
   end
 
