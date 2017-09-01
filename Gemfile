c2d520b3d886817cb931cac8f4906a0621f9cd55
tassaddar <tassaddar-90@yandex.ru>
2017-09-01 19:58:53 +0300

    add signup implementation skeleton

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'rails', '~> 5.1.2'
 gem 'bootstrap-sass', '3.3.7'
 gem 'rails-controller-testing', '1.0.2'
 gem 'wdm', '>= 0.1.0'
+gem 'bcrypt', git: 'https://github.com/codahale/bcrypt-ruby.git', :require => 'bcrypt'
 
 # Use sqlite3 as the database for Active Record
 #gem 'sqlite3'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase! }
     validates :name, presence: true, length: { maximum: 50 } 
-    validates :email, presence: true, length: { maximum: 255 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                      format: { with: VALID_EMAIL_REGEX },
+                      uniqeness: true { case_sensitive: false }
+
+    has_secure_password # bicrypt gem's helper method, it allows to use "authenticate method which returns either false or the user object itself"
+    validates :password, presence: true, length: { minimum: 6 }
 end
