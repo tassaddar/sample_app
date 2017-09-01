d317bbb4190055217b68768b04adc88c777b4da4
tassaddar <tassaddar-90@yandex.ru>
2017-09-01 22:05:17 +0300

    fixed user vodel validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
-                      uniqeness: true { case_sensitive: false }
+                      uniqueness: true { case_sensitive: false }
 
     has_secure_password # bicrypt gem's helper method, it allows to use "authenticate method which returns either false or the user object itself"
     validates :password, presence: true, length: { minimum: 6 }
