e98aa62b90962c0055ecbcfcfe2ea879eb3d6e50
tassaddar <tassaddar-90@yandex.ru>
2017-09-13 20:01:48 +0300

    add signup page (in progress...)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-    before_save { self.email = email.downcase! }
+    #before_save { self.email = email.downcase! }
     validates :name, presence: true, length: { maximum: 50 } 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
-
-    has_secure_password # bicrypt gem's helper method, it allows to use "authenticate method which returns either false or the user object itself"
     validates :password, presence: true, length: { minimum: 6 }
+    has_secure_password # bicrypt gem's helper method, it allows to use "authenticate method which returns either false or the user object itself"
+    
 end
